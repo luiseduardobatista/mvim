1f75c3453ea96987a46cbcd42204f9714ec9f703
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-08-22 11:35:59 -0300

    feat: blink.cmp enable auto show

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -35,12 +35,20 @@ now(function()
 			nerd_font_variant = "mono",
 		},
 		completion = {
-			-- By default, you may press `<c-space>` to show the documentation.
-			-- Optionally, set `auto_show = true` to show the documentation after a delay.
-			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+			documentation = { auto_show = true, auto_show_delay_ms = 200 },
+		},
+		accept = {
+			auto_brackets = {
+				enabled = true,
+			},
+		},
+		menu = {
+			draw = {
+				treesitter = { "lsp" },
+			},
 		},
 		sources = {
-			default = { "lsp", "path", "snippets", "lazydev" },
+			default = { "lsp", "path", "snippets", "buffer", "lazydev" },
 			providers = {
 				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 			},
