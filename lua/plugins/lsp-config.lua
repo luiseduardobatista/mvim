5ae1c6311825d1d78fd46b16e740a79712ef17fc
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-08-21 21:50:29 -0300

    feat: add lsp config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,216 @@
+-- LSP Plugins
+local add, later = MiniDeps.add, MiniDeps.later
+
+add('folke/lazydev.nvim')
+add('neovim/nvim-lspconfig')
+add('mason-org/mason.nvim')
+add('mason-org/mason-lspconfig.nvim')
+add('WhoIsSethDaniel/mason-tool-installer.nvim')
+add('j-hui/fidget.nvim')
+add('saghen/blink.cmp')
+add('ibhagwan/fzf-lua')
+
+-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+-- used for completion, annotations and signatures of Neovim apis
+later(function()
+  -- Monkeypatch in a PR to remove a call to the deprecated `client.notify`
+  -- function.
+  --
+  -- See: https://github.com/folke/lazydev.nvim/pull/106
+  local config = require("lazydev.config")
+  config.have_0_11 = vim.fn.has("nvim-0.11") == 1
+
+  local lsp = require("lazydev.lsp")
+  lsp.update = function(client)
+    lsp.assert(client)
+    if config.have_0_11 then
+      client:notify("workspace/didChangeConfiguration", {
+        settings = { Lua = {} },
+      })
+    else
+      client.notify("workspace/didChangeConfiguration", {
+        settings = { Lua = {} },
+      })
+    end
+  end
+
+  require("lazydev").setup {
+    library = {
+      -- Load luvit types when the `vim.uv` word is found
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    },
+  }
+end)
+
+-- Useful status updates for LSP.
+later(function()
+  require('fidget').setup({})
+end)
+
+later(function()
+  require('fzf-lua').setup({})
+end)
+
+-- Automatically install LSPs and related tools to stdpath for Neovim
+-- Mason must be loaded before its dependents so we need to set it up here.
+later(function()
+  require('mason').setup({})
+end)
+
+local servers = {
+  mason = {
+    lua_ls = {
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = 'Replace',
+          },
+          -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+          -- diagnostics = { disable = { 'missing-fields' } },
+        },
+      },
+    },
+  },
+  others = {},
+}
+
+-- Ensure the servers and tools above are installed
+-- To check the current status of installed tools and/or manually install
+-- other tools, you can run
+--   :Mason
+later(function()
+  local ensure_installed = vim.tbl_keys(servers.mason or {})
+  vim.list_extend(ensure_installed, {
+    'stylua', -- Used to format Lua code
+  })
+  require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+end)
+
+-- After configuring our language servers, we now enable them
+later(function()
+  require('mason-lspconfig').setup {
+    handlers = {
+      function(server_name)
+        local server_config = servers.mason[server_name] or servers.others[server_name] or {}
+        require('lspconfig')[server_name].setup(server_config)
+      end,
+    },
+  }
+end)
+
+-- This function gets run when an LSP attaches to a particular buffer.
+later(function()
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+    callback = function(event)
+      -- In this case, we create a function that lets us more easily define mappings specific
+      -- for LSP related items. It sets the mode, buffer and description for us each time.
+      local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      end
+
+      -- Rename the variable under your cursor.
+      -- Most Language Servers support renaming across files, etc.
+      map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+      -- Execute a code action, usually your cursor needs to be on top of an error
+      -- or a suggestion from your LSP for this to activate.
+      map('gra', function() require('fzf-lua').lsp_code_actions() end, '[G]oto Code [A]ction', { 'n', 'x' })
+
+      -- Find references for the word under your cursor.
+      map('grr', function() require('fzf-lua').lsp_references() end, '[G]oto [R]eferences')
+
+      -- Jump to the implementation of the word under your cursor.
+      -- Useful when your language has ways of declaring types without an actual implementation.
+      map('gri', function() require('fzf-lua').lsp_implementations() end, '[G]oto [I]mplementation')
+
+      -- Jump to the definition of the word under your cursor.
+      -- This is where a variable was first declared, or where a function is defined, etc.
+      -- To jump back, press <C-t>.
+      map('grd', function() require('fzf-lua').lsp_definitions() end, '[G]oto [D]efinition')
+
+      -- WARN: This is not Goto Definition, this is Goto Declaration.
+      -- For example, in C this would take you to the header.
+      map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+      -- Fuzzy find all the symbols in your current document.
+      -- Symbols are things like variables, functions, types, etc.
+      map('gO', function() require('fzf-lua').lsp_document_symbols() end, 'Open Document Symbols')
+
+      -- Fuzzy find all the symbols in your current workspace.
+      -- Similar to document symbols, except searches over your entire project.
+      map('gW', function() require('fzf-lua').lsp_workspace_symbols() end, 'Open Workspace Symbols')
+
+      -- Jump to the type of the word under your cursor.
+      -- Useful when you're not sure what type a variable is and you want to see
+      -- the definition of its *type*, not where it was *defined*.
+      map('grt', function() require('fzf-lua').lsp_type_definitions() end, '[G]oto [T]ype Definition')
+
+      -- Toggle to show/hide diagnostic messages
+      map('<leader>td', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, '[T]oggle [D]iagnostics')
+
+      -- The following two autocommands are used to highlight references of the
+      -- word under your cursor when your cursor rests there for a little while.
+      -- See `:help CursorHold` for information about when this is executed
+      --
+      -- When you move your cursor, the highlights will be cleared (the second autocommand).
+      local client = vim.lsp.get_client_by_id(event.data.client_id)
+      if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+        local highlight_augroup = vim.api.nvim_create_augroup('user-lsp-highlight', { clear = false })
+        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+          buffer = event.buf,
+          group = highlight_augroup,
+          callback = vim.lsp.buf.document_highlight,
+        })
+
+        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+          buffer = event.buf,
+          group = highlight_augroup,
+          callback = vim.lsp.buf.clear_references,
+        })
+
+        vim.api.nvim_create_autocmd('LspDetach', {
+          group = vim.api.nvim_create_augroup('user-lsp-detach', { clear = true }),
+          callback = function(event2)
+            vim.lsp.buf.clear_references()
+            vim.api.nvim_clear_autocmds { group = 'user-lsp-highlight', buffer = event2.buf }
+          end,
+        })
+      end
+
+      -- The following code creates a keymap to toggle inlay hints in your
+      -- code, if the language server you are using supports them
+      --
+      -- This may be unwanted, since they displace some of your code
+      if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+      end
+    end,
+  })
+end)
+
+-- Diagnostic Config
+-- See :help vim.diagnostic.Opts
+later(function()
+  vim.diagnostic.config {
+    severity_sort = true,
+    float = { border = 'rounded', source = 'if_many' },
+    signs = vim.g.have_nerd_font and {
+      text = {
+        [vim.diagnostic.severity.ERROR] = '󰅚 ',
+        [vim.diagnostic.severity.WARN] = '󰀪 ',
+        [vim.diagnostic.severity.INFO] = '󰋽 ',
+        [vim.diagnostic.severity.HINT] = '󰌶 ',
+      },
+    } or {},
+    virtual_text = {
+      source = 'if_many',
+      spacing = 2,
+    },
+    -- Display multiline diagnostics as virtual lines
+    -- virtual_lines = true,
+  }
+end)
+
+-- vim: ts=2 sts=2 sw=2 et
