0408a7a68895fcc821b538b5f90e66f73e5596a0
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-08-23 17:58:28 -0300

    feat: improve rustaceanvim config

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,5 +1,4 @@
 local add, later = MiniDeps.add, MiniDeps.later
-
 add({ source = "mrcjkb/rustaceanvim", checkout = "master" })
 add({ source = "Saecki/crates.nvim" })
 
@@ -18,10 +17,51 @@ later(function()
 
 	vim.g.rustaceanvim = {
 		server = {
+			on_attach = function(_, bufnr)
+				vim.keymap.set("n", "<leader>cR", function()
+					vim.cmd.RustLsp("codeAction")
+				end, { desc = "Rust Code Action", buffer = bufnr })
+
+				vim.keymap.set("n", "<leader>dr", function()
+					vim.cmd.RustLsp("debuggables")
+				end, { desc = "Rust Debuggables", buffer = bufnr })
+			end,
 			default_settings = {
 				["rust-analyzer"] = {
 					cargo = {
 						allFeatures = true,
+						loadOutDirsFromCheck = true,
+						buildScripts = {
+							enable = true,
+						},
+					},
+					checkOnSave = true,
+					check = {
+						command = "clippy",
+					},
+					diagnostics = {
+						enable = true,
+					},
+					procMacro = {
+						enable = true,
+						ignored = {
+							["async-trait"] = { "async_trait" },
+							["napi-derive"] = { "napi" },
+							["async-recursion"] = { "async_recursion" },
+						},
+					},
+					files = {
+						excludeDirs = {
+							".direnv",
+							".git",
+							".github",
+							".gitlab",
+							"bin",
+							"node_modules",
+							"target",
+							"venv",
+							".venv",
+						},
 					},
 				},
 			},
